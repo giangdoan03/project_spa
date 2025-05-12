3e3989cea5bc842d3e4cd6b83ddbb5e5c7ce7a52
Doan Giang <dvg8.vcc@gmail.com>
2025-05-12 23:31:04 +0700

    update

--- fe/src/api/person.js
+++ fe/src/api/person.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     withCredentials: true, // 👈 Bắt buộc để giữ session
 })
 
