22db48e3f42da231631667b853f6b7a3e808bf5e
Giang Đoàn <doangiang665@gmail.com>
2025-05-14 15:08:59 +0700

    update

--- fe/src/components/templates/products/index.js
+++ fe/src/components/templates/products/index.js
@@ -1,25 +1,25 @@
-// src/components/templates/index.js
+import { ASSET_URL } from '@/config/env';
+
 export default [
     {
         id: 'tpl-1',
         title: 'Mẫu 1',
         description: 'Giao diện vàng nhẹ',
-        thumbnail: 'https://assets.labit365.com/image/1745030161_1629c049dd5304b986df.jpg',
+        thumbnail: `${ASSET_URL}/image/1745030161_1629c049dd5304b986df.jpg`,
         component: () => import('./Tpl1.vue'),
     },
     {
         id: 'tpl-2',
         title: 'Mẫu 2',
         description: 'Giao diện tím sang trọng',
-        thumbnail: 'https://assets.labit365.com/image/1745030182_9c8ac5def4099a010923.jpg',
+        thumbnail: `${ASSET_URL}/image/1745030182_9c8ac5def4099a010923.jpg`,
         component: () => import('./Tpl2.vue'),
     },
     {
         id: 'tpl-3',
         title: 'Mẫu doanh nghiệp hiện đại',
         description: 'Giao diện sáng, hiển thị chuyên nghiệp, bố cục rõ ràng',
-        thumbnail: 'https://assets.labit365.com/image/1745030199_204f7c732930f2e30a52.jpg',
+        thumbnail: `${ASSET_URL}/image/1745030199_204f7c732930f2e30a52.jpg`,
         component: () => import('./Tpl3.vue'),
     },
-    // ...
-]
+];
